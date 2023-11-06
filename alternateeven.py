a02f502e400ca0003fb172c43ffebfabbbbfbfe3
Tushar087-jpmc <62515266+Tushar087-jpmc@users.noreply.github.com>
2023-11-06 08:18:18 +0530

    alternate even

--- alternateeven.py
+++ alternateeven.py
@@ -2,3 +2,8 @@
 
 for num in range(20, 141, 4):
     print(num)
+
+#Write a program to print alternate even numbers from 20 to 140. Like (22,26,30...)
+
+for num in range(22, 141, 4):
+    print(num)
