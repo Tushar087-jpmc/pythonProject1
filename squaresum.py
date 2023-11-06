2e68d0085b6010aae81033d226b1fe3d6a6dac8e
Tushar087-jpmc <62515266+Tushar087-jpmc@users.noreply.github.com>
2023-11-06 08:39:57 +0530

    sumsquarenum

---
+++ squaresum.py
@@ -0,0 +1,11 @@
+# Initialize variables
+start = 5
+end = 100
+total = 0
+
+# Calculate the sum of squares
+for num in range(start, end + 1):
+    total += num**2
+
+# Print the result
+print("The sum of squares from 5^2 to 100^2 is:", total)
