6d1a5996ba44d487c9912be3329a9e5f55fd211b
Tushar087-jpmc <62515266+Tushar087-jpmc@users.noreply.github.com>
2023-11-06 08:22:28 +0530

    patternprint

---
+++ seriesprint.py
@@ -0,0 +1,5 @@
+#  Print following series 2*3,3*4,4*5,......16*17 (Print in two ways – patter & multiplied value)
+for num1 in range(2, 17):
+    num2 = num1 + 1
+    print(f"{num1}*{num2}", end=", ")
+
